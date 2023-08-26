935ba7688eec9e9cd5f1dafb65f3c3a31e64bb7b
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2023-08-26 10:37:59 +0900

    田上

--- Assets/TagamiMotohiro/Script/GestureArea.cs
+++ Assets/TagamiMotohiro/Script/GestureArea.cs
@@ -9,6 +9,8 @@ public class GestureArea : MonoBehaviour
      MocopiPlayerWork Player;
     [SerializeField]
     float radius;
+    [SerializeField]
+    KeyObject keyItem;
     // Start is called before the first frame update
     void Start()
     {
