b4bba743fb6992d987dbbfa66ae5a695998cc893
Ryota-ika <86831039+Ryota-ika@users.noreply.github.com>
2023-09-20 10:39:22 +0900

    ryotatakahshi ナビキャラ画面左上遷移

--- Assets/c#script/CharacterCtrl.cs
+++ Assets/c#script/CharacterCtrl.cs
@@ -12,10 +12,6 @@ public class CharacterCtrl : MonoBehaviour
     [SerializeField]
     private Transform naviPos;
     [SerializeField]
-    private Transform ovrCameraRig;
-    [SerializeField]
-    private Vector3 offSet = new Vector3 (-0.5f, 0.5f, 0.0f);
-    [SerializeField]
     float smoothTime = 0.5f;
     [SerializeField]
     float rotateTime;
@@ -29,9 +25,9 @@ public class CharacterCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 target_pos = ovrCameraRig.position + offSet;
+        Vector3 target_pos = naviPos.position;
         transform.position=Vector3.SmoothDamp(transform.position,target_pos,ref velocity,smoothTime);
-        Vector3 loockPos = ovrCameraRig.position - transform.position;
+        Vector3 loockPos = targetPlayer.position - transform.position;
         loockPos.y = 0;
         if (loockPos != Vector3.zero)
 		{
