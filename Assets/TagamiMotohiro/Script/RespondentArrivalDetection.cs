8a17a1b50d84bba0eea5f97cdcb3bb9d6b79dfac
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2024-01-10 19:51:13 +0900

    プレイヤー到着検知用スクリプト作成

---
+++ Assets/TagamiMotohiro/Script/RespondentArrivalDetection.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+// ジェスチャー回答側が特定の位置に到達したのを検知するクラス
+public class RespondentArrivalDetection : MonoBehaviourPunCallbacks{
+    [SerializeField]
+    GameObject stanbyPanel;
+    [SerializeField]
+    float detectionRange;
+    [SerializeField]
+    Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    [PunRPC]
+	// 回答側にジェスチャー問題を開示する
+	void GestureDisclosure()
+	{
+        stanbyPanel.SetActive(false);
+	}
+
+    IEnumerator ArrivalDetection()
+	{
+        while (Vector3.Distance(player.position,transform.position) > detectionRange)
+		{
+            yield return null;
+		}
+        photonView.RPC(nameof(GestureDisclosure), RpcTarget.All);
+        Destroy(gameObject);
+	}
+}
