6596311aee968a35d24bf55cb652c6e69e6e0635
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2024-01-13 14:52:20 +0900

    (見た目は仮ですが)ジェスチャー回答側が到着したら出題側に問題を提示するシステムを作成しました

--- Assets/TagamiMotohiro/Script/RespondentArrivalDetection.cs
+++ Assets/TagamiMotohiro/Script/RespondentArrivalDetection.cs
@@ -13,7 +13,7 @@ public class RespondentArrivalDetection : MonoBehaviourPunCallbacks{
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(ArrivalDetection());
     }
 
     // Update is called once per frame
